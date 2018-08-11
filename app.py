17f2165cf8c9f188c77e921970d8e40a4507061a
Glass Rye <glass4sites@gmail.com>
2018-08-10 21:19:10 -0700

    yerp

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app.config['secret-key'] = 'jbk'
 
 def main():
     # TODO: Move this to a configuration or environment file (pref environment)
-    API_KEY = '934114801411702784-49rSicC72DghfHWO0Vsowns36yzCwvw'
-    API_SECRET = '08O6fpFwONnywJ258Q7eMq4wkP4OqdfXWIcn0o2vYqT1o'
+    API_KEY = config.state
+    API_SECRET = config.state
 
     my_update = TwitterBase('https://api.twitter.com',
                             api_key=API_KEY,
